5e7094e49ea591fc929acbe45227119b4193c53d
Lazar Georgiev <Lazar Georgiev>
2016-11-20 00:55:19 +0200

    Scraper further optimized; JSON complete

--- Scraper/server.js
+++ Scraper/server.js
@@ -1,59 +1,129 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
-var domainLink = "http://zoomtravel.bg/wat-usa";
+var domainLink1 = "http://zoomtravel.bg";
+var nRequests = 0;
 
-for(var i = 0; i <=3; i++){
-request(domainLink + "?page="+ i, function(error, response, body) {
-  if(error) {
-    console.log("Error: " + error);
-  }
-  console.log("Status code: " + response.statusCode);
+	function handleRequests(error, response, body){
+			  if(error) {
+			    console.log("Error: " + error);
+			  }
+			  console.log("Status code: " + response.statusCode);
 
-  var $ = cheerio.load(body);
-  //var json = { city : "", state : "", position : "", wage : "", workinghours : "", startdate : "", link : ""};
+			  var $ = cheerio.load(body);
+			  $('.view-work-offers .work-offer').each(function( index ) {
+			    	var temp = $(this).find('.city').text().trim();
+					temp = temp.replace(", ", ",");
 
-  $('.view-work-offers .work-offer').each(function( index ) {
-  		var id = new Date().valueOf();
-    	var temp = $(this).find('.city').text().trim();
-		temp = temp.replace(", ", ",");
+			       	var state = temp.substring(temp.indexOf(",")+1, temp.length);
+			       	var city =  temp.substring(0, temp.indexOf(","));
 
-       	var state = temp.substring(temp.indexOf(",")+1, temp.length);
-       	var city =  temp.substring(0, temp.indexOf(","));
+			       	var position = $(this).find('.position').text().trim();
+			       	var wage = $(this).find(".paymant span").text().trim();
+			       	wage = wage.substring(1,wage.indexOf(" "));
 
-       	var position = $(this).find('.position').text().trim();
-       	var wage = $(this).find(".paymant span").text().trim();
-       	wage = wage.substring(1,wage.indexOf(" "));
+			       	var startdate = $(this).find('.date-display-single').text().trim();
+			       	if(startdate.includes(",")) startdate = startdate.substring(0, startdate.indexOf(","));
+			       	var enddate = "01.09.2017";
 
-       	var startdate = $(this).find('.date-display-single').text().trim();
-       	var enddate = "01.09.2017";
+			       	var workinghours = $(this).find('p.others').text().trim();
+			       	workinghours = workinghours.replace("h", "");
+			       	workinghours = workinghours.substring(workinghours.lastIndexOf(" ")+1, workinghours.length);
+			      	var overtime;
+			      	if(workinghours.includes("+") === true){
+			      		workinghours = workinghours.substring(0,workinghours.indexOf("+"));
+			      		overtime = true;
+			      	}
+			      	else if(workinghours.includes("-")){
+			      		overtime = false;
+			      		var temp2 = workinghours.substring(0, workinghours.indexOf("-"));
+			      		workinghours = workinghours.substring(workinghours.indexOf("-")+1, workinghours.length);
+			      		workinghours = (parseInt(temp2) + parseInt(workinghours)) /2;
 
-       	var workinghours = $(this).find('p.others').text().trim();
-       	workinghours = workinghours.replace("h", "");
-       	workinghours = workinghours.substring(workinghours.lastIndexOf(" ")+1, workinghours.length);
-      	var overtime;
-      	if(workinghours.includes("+") === true){
-      		workinghours = workinghours.substring(0,workinghours.indexOf("+"));
-      		overtime = true;
-      	}
-      	else if(workinghours.includes("-")){
-      		overtime = false;
-      		var temp2 = workinghours.substring(0, workinghours.indexOf("-"));
-      		workinghours = workinghours.substring(workinghours.indexOf("-")+1, workinghours.length);
-      		workinghours = (parseInt(temp2) + parseInt(workinghours)) /2;
+			      	}
+			       	
+			       	var link = $(this).find('a').attr('href');
+			       	link = domainLink1 + link;
 
-      	}
-       	
-       	var link = $(this).find('a').attr('href');
-       	link = domainLink + link;
+				    fs.appendFileSync('output.json',
+				    	'{\n\t"CityName": "' + city + '",\n' + '\t"StateName": "' +  state + '",\n'
+				    	+ '\t"Position": "' + position + '",\n' + '\t"Wage": ' + parseInt(wage) + ',\n'
+				    	+ '\t"WorkingHours": ' + parseInt(workinghours) + ',\n'+ '\t"Overtime": "' + overtime + '",\n'
+				    	+ '\t"StartDate": "' + startdate + '",\n'+ '\t"EndDate": "' + enddate + '",\n'
+				    	 + '\t"Link": "' + link + '",\n},\n');
+					});
 
-    fs.appendFileSync('output.json',
-    	'{\n\t"id": ' + parseInt(id) + ',\n' + '\t"city": "' +  city + '",\n' + '\t"state": "' +  state + '",\n'
-    	+ '\t"position": "' + position + '",\n' + '\t"wage": ' + parseInt(wage) + ',\n'
-    	+ '\t"workinghours": ' + parseInt(workinghours) + ',\n'+ '\t"overtime": "' + overtime + '",\n'
-    	+ '\t"startdate": "' + startdate + '",\n'+ '\t"enddate": "' + enddate + '",\n'
-    	 + '\t"link": "' + link + '",\n},\n');
+				++nRequests;
+		  		if(nRequests>2) return;
+			    request(domainLink1 + "/wat-usa/?page=" + nRequests, handleRequests);
+		  }
+		  request(domainLink1 + "/wat-usa/?page=" + nRequests, handleRequests);
 
-  });
-});
-}
+
+
+
+
+var domainLink = "http://watabroad.com/job-offers-USA?product_id=";
+var nRequest = 107;
+
+		function handleRequest(error, response, body){
+			 if(error) {
+			    console.log("Error: " + error + " Request # " + (nRequest-106));
+			  }
+			  console.log("Status code: " + response.statusCode + " Request # " + (nRequest-106));
+
+			  var $ = cheerio.load(body);
+			  if($('#offer-table').length == 1){
+
+				var state = $('#offer-table tr:nth-child(1) td:nth-child(1) span:nth-child(2)').text().trim();
+			   	var city =  $('#offer-table tr:nth-child(1) td:nth-child(2) span:nth-child(2)').text().trim();
+			   	state = state.substring(state.indexOf("(")+1, state.indexOf(")"));
+			   	var position = $('#offer-table tr:nth-child(2) td:nth-child(1) span:nth-child(2)').text().trim();
+			   	if(position.includes(","))	position = position.substring(0, position.indexOf(","));
+			   	
+			   	var wage = $('#offer-table tr:nth-child(3) td:nth-child(1) span:nth-child(2)').text().trim();
+			   	wage = wage.replace('$ ', '');
+			   	wage = wage.replace('$' , '');
+			   	wage = wage.replace('+', '');
+			   	wage = wage.replace('tips', '');
+			   	wage = wage.replace('tip', '');
+			   	wage = wage.replace(',', '.');
+			   	if(wage.includes(';')) wage.substring(wage.lastIndexOf(" ")+1, wage.length);
+			   	else if(wage.includes(' ')) wage = wage = wage.substring(0, wage.indexOf(" "));
+
+			   	var startdate = "20.05.2017";
+			   	var enddate = "15.09.2017";
+
+			   	var workinghours = $('#offer-table tr:nth-child(3) td:nth-child(2) span:nth-child(2)').text().trim();
+
+			   	workinghours = workinghours.replace("h", "");
+			   	workinghours = workinghours.replace(" ", "");
+
+			  	var overtime;
+			  	if(workinghours.length == 2) overtime = false;
+			  	if(workinghours.includes("+") === true){
+			  		workinghours = workinghours.substring(0,workinghours.indexOf("+"));
+			  		overtime = true;
+			  	}
+			  	else if(workinghours.includes("-")){
+			  		overtime = false;
+			  		var temp2 = workinghours.substring(0, workinghours.indexOf("-"));
+			  		workinghours = workinghours.substring(workinghours.indexOf("-")+1, workinghours.length);
+			  		workinghours = (parseInt(temp2) + parseInt(workinghours)) /2;
+
+			  	}
+			    var link = domainLink + nRequest;
+			    
+
+			    fs.appendFileSync('output.json',
+			    	'{\n\t"CityName": "' + city + '",\n' + '\t"StateName": "' +  state + '",\n'
+			    	+ '\t"Position": "' + position + '",\n' + '\t"Wage": ' + parseInt(wage) + ',\n'
+			    	+ '\t"WorkingHours": ' + parseInt(workinghours) + ',\n'+ '\t"Overtime": "' + overtime + '",\n'
+			    	+ '\t"StartDate": "' + startdate + '",\n'+ '\t"EndDate": "' + enddate + '",\n'
+			    	 + '\t"Link": "' + link + '",\n},\n');
+			}
+				++nRequest;
+		  		if(nRequest>520) return;
+			    request(domainLink + nRequest, handleRequest);
+		  }
+		  request(domainLink + 107, handleRequest);
